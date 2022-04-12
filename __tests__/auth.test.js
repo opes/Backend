5f067eb5a28bad8d7bd64e2bb9869101f0daaf91
Libbi Dunham <Libbi.Dunham25@gmail.com>
2022-04-12 14:40:10 -0700

    wrote test and controller for signing out a user

--- __tests__/auth.test.js
+++ __tests__/auth.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const GithubUser = require('../lib/models/GithubUser');
 
 jest.mock('../lib/utils/github');
 
@@ -27,5 +28,25 @@ describe('TravelBackend routes', () => {
       .get('/api/v1/github/login/callback?code=42')
       .redirects(1);
     expect(res.req.path).toEqual('/api/v1/trips');
+  });
+
+  it('should sign out a user', async () => {
+    const agent = request.agent(app);
+    await GithubUser.insert({
+      username: 'fake_github_user',
+      email: 'not-real@example.com',
+      avatar: 'https://www.placecage.com/gif/300/300',
+    });
+    await agent.post('/api/v1/github/login').send({
+      username: 'fake_github_user',
+      email: 'not-real@example.com',
+      avatar: 'https://www.placecage.com/gif/300/300',
+    });
+
+    const res = await agent.delete('/api/v1/github/login');
+    expect(res.body).toEqual({
+      success: true,
+      message: 'Successfully signed out',
+    });
   });
 });
