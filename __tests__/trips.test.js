bebb5fe5a206d542ea7426faa1bb76405e27f98f
Libbi Dunham(she/her) <89104000+Libbi-Dunham@users.noreply.github.com>
2022-04-13 10:05:07 -0700

    Libbi (#3)
    * wrote out users sql table
    
    * made another env added our client id/secret and update yml file
    
    * wrote out and got redirects to oauth when logged in test passing
    
    * test, controller written for callback as well as githubuser insert model
    
    * working on getting callback test to pass. Realized we need to implement trips table so redirect works
    
    * created trips sql table
    
    * updated sql file added a junction table
    
    * wrote out authenticate file
    
    * got auth test callback to pass(fixed an error) now working on trips test get lists of trips to pass
    
    * got trips test to pass had to update sql file
    
    * wrote test and controller for signing out a user
    
    * added back in create user test in trip file and handing merge conflicts
    
    * wrote test,controller, and model for get trips by id
    
    * wrote test, controller, model for delete trips
    
    * wrote test, controller, model for update trips
    
    Co-authored-by: NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>

--- __tests__/trips.test.js
+++ __tests__/trips.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Trip = require('../lib/models/Trip');
 
 jest.mock('../lib/utils/github');
 
@@ -37,5 +38,64 @@ describe('TravelBackend routes', () => {
     res = await agent.get('/api/v1/trips');
     expect(res.status).toEqual(200);
     expect(res.body).toEqual([...trips]);
+  });
+
+  it('allows an authenticated user to create a new trip', async () => {
+    const agent = request.agent(app);
+
+    let res = await agent.post('/api/v1/trips');
+    expect(res.status).toEqual(401);
+
+    await agent.get('/api/v1/github/login/callback?code=42').redirects(1);
+    const trip = {
+      location: 'Spain',
+      startDate: '9/9/2022',
+      endDate: '9/21/2022',
+    };
+    res = await agent.post('/api/v1/trips').send(trip);
+    expect(res.body).toEqual({ id: expect.any(String), ...trip });
+  });
+
+  it('should be able to list a trip by id', async () => {
+    const trip = await Trip.insert({
+      location: 'vegas',
+      startDate: '4/29/2022',
+      endDate: '5/12/2022',
+    });
+    const res = await request(app).get(`/api/v1/trips/${trip.id}`);
+    expect(res.body).toEqual(trip);
+  });
+
+  it('should be able to delete a trip', async () => {
+    const trip = await Trip.insert({
+      location: 'vegas',
+      startDate: '4/29/2022',
+      endDate: '5/12/2022',
+    });
+    const res = await request(app).delete(`/api/v1/trips/${trip.id}`);
+    expect(res.body).toEqual(trip);
+    expect(await Trip.getById(trip.id)).toBeNull();
+  });
+
+  it('should update a trip', async () => {
+    const trip = await Trip.insert({
+      location: 'vegas',
+      startDate: '4/29/2022',
+      endDate: '5/12/2022',
+    });
+    const res = await request(app).patch(`/api/v1/trips/${trip.id}`).send({
+      location: 'Bali',
+      startDate: '7/29/2022',
+      endDate: '8/12/2022',
+    });
+
+    const expected = {
+      id: expect.any(String),
+      location: 'Bali',
+      startDate: '7/29/2022',
+      endDate: '8/12/2022',
+    };
+    expect(res.body).toEqual(expected);
+    expect(await Trip.getById(trip.id)).toEqual(expected);
   });
 });
