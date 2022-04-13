f704cfa73609786dc2f820a5c78839f8a34c3cb7
Libbi Dunham(she/her) <89104000+Libbi-Dunham@users.noreply.github.com>
2022-04-12 18:01:40 -0700

    Libbi (#1)
    * wrote out users sql table
    
    * made another env added our client id/secret and update yml file
    
    * wrote out and got redirects to oauth when logged in test passing
    
    * test, controller written for callback as well as githubuser insert model
    
    * working on getting callback test to pass. Realized we need to implement trips table so redirect works
    
    * created trips sql table
    
    * updated sql file added a junction table
    
    * wrote out authenticate file
    
    * got auth test callback to pass(fixed an error) now working on trips test get lists of trips to pass
    
    * got trips test to pass had to update sql file
    
    * wrote test and controller for signing out a user
    
    Co-authored-by: NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>

---
+++ __tests__/auth.test.js
@@ -0,0 +1,52 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+const GithubUser = require('../lib/models/GithubUser');
+
+jest.mock('../lib/utils/github');
+
+describe('TravelBackend routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('redirect to the github oauth when logged in', async () => {
+    const req = await request(app).get('/api/v1/github/login');
+    expect(req.header.location).toMatch(
+      /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/login\/callback/i
+    );
+  });
+
+  it('redirects user to trips after successful login', async () => {
+    const res = await request
+      .agent(app)
+      .get('/api/v1/github/login/callback?code=42')
+      .redirects(1);
+    expect(res.req.path).toEqual('/api/v1/trips');
+  });
+
+  it('should sign out a user', async () => {
+    const agent = request.agent(app);
+    await GithubUser.insert({
+      username: 'fake_github_user',
+      email: 'not-real@example.com',
+      avatar: 'https://www.placecage.com/gif/300/300',
+    });
+    await agent.post('/api/v1/github/login').send({
+      username: 'fake_github_user',
+      email: 'not-real@example.com',
+      avatar: 'https://www.placecage.com/gif/300/300',
+    });
+
+    const res = await agent.delete('/api/v1/github/login');
+    expect(res.body).toEqual({
+      success: true,
+      message: 'Successfully signed out',
+    });
+  });
+});
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,37 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS trips CASCADE;
+DROP TABLE IF EXISTS users_trips CASCADE;
+
+CREATE TABLE users (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT,
+    avatar TEXT
+);
+
+CREATE TABLE trips (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    location TEXT NOT NULL,
+    start_date DATE,
+    end_date DATE,
+    users BIGINT NOT NULL REFERENCES users(id)
+);
+
+CREATE TABLE users_trips (
+    users_id BIGINT REFERENCES users(id),
+    trips_id BIGINT REFERENCES trips(id)
+);
+
+INSERT INTO
+users (username, email, avatar)
+VALUES
+('user.user', 'user.test@test.com', 'https://avatars.githubusercontent.com/u/87677895?v=4'),
+('user.user2', 'user.test@test2.com', 'https://avatars.githubusercontent.com/u/68452618?v=4');
+
+INSERT INTO
+ trips (location, start_date, end_date, users)
+VALUES
+('vegas', '4/29/2022', '5/12/2022', 1),
+('italy', '6/19/2022', '7/20/2022', 2);
