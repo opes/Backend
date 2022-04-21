79364ee3ba32f9bc0cc202eac95cbcc2cf391f0b
Brett Seifried <seifriedbrett@gmail.com>
2022-04-21 12:07:46 -0700

    Brett4 (#41)
    * TA's saving the day for context and private routes
    
    * created lodging table, passing instert
    
    * half way done with grabbing ID, need to set up /:id for pulilng info
    
    * passing get hotel by Id
    
    * update hotel by ID passing test
    
    * loding back end working
    
    * troubleshooting push
    
    * fixed setup.sql
    
    * Yelp API pushing to front end
    
    * updated limit to 3 yelp req
    
    * n.map issue
    
    * heorku update
    
    * Gabe's changes
    
    * lodging back end
    
    * fixed test with new tripsId
    
    * added guest_id for delete
    
    * lodging tests updated, delete added
    
    * lodging working for delete and add

--- __tests__/lodging.test.js
+++ __tests__/lodging.test.js
@@ -68,9 +68,21 @@ describe('Lodging routes', () => {
       tripsId: null,
       zip: '12345',
     });
-    const res = await request(app).patch(`/api/v1/lodging/${lodging.id}`).send({
-      nameOfPlace: 'Updated Hotel',
-    });
+    const res = await request(app)
+      .patch(`/api/v1/lodging/${lodging.id}`)
+      .send({
+        nameOfPlace: 'Updated Hotel',
+        contactInfo: 'Test Info',
+        pricePerNight: '100',
+        checkIn: '1/1/2000',
+        checkOut: '1/4/2000',
+        address1: '123 Test Ave',
+        address2: '',
+        city: 'Test City',
+        state: 'WA',
+        tripsId: expect.any(String),
+        zip: '12345',
+      });
 
     const expected = {
       id: expect.any(String),
@@ -88,5 +100,24 @@ describe('Lodging routes', () => {
     };
     expect(res.body).toEqual(expected);
     expect(await Lodging.getHotelById(lodging.id)).toEqual(expected);
+  });
+
+  it('should be able to delete a booking', async () => {
+    const lodging = await Lodging.insert({
+      nameOfPlace: 'Test Hotel',
+      contactInfo: 'Test Info',
+      pricePerNight: '100',
+      checkIn: '1/1/2000',
+      checkOut: '1/4/2000',
+      address1: '123 Test Ave',
+      address2: '',
+      city: 'Test City',
+      state: 'WA',
+      tripsId: null,
+      zip: '12345',
+    });
+    const res = await request(app).delete(`/api/v1/lodging/${lodging.id}`);
+    expect(res.body).toEqual(lodging);
+    expect(await Lodging.deleteById(lodging.id)).toBeNull();
   });
 });
